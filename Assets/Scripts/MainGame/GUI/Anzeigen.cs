6df9af605594f3446adb3eeb679df65bb81cb67f
Noah20102021 <schledernoah0@gmail.com>
2023-11-25 13:44:34 +0100

    Noah

---
+++ Assets/Scripts/MainGame/GUI/Anzeigen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Anzeigen : MonoBehaviour
+{
+    public GameObject Kontostand;
+    TextMeshProUGUI KontostandTMP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        KontostandTMP = Kontostand.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string Test;
+        Test = Speichern.Kontostand.ToString();
+        Debug.Log(Test);
+        KontostandTMP.text = Test;
+    }
+}
---
+++ Assets/Scripts/MainGame/Speichern/Speichern.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Speichern
+{
+    public static long Kontostand = 20;
+    public static Time Date;
+    public static void hallo()
+    {
+        Date = new Time();
+    }
+    
+}
